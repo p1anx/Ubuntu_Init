082b23ddf2f31600a2bfd38ca1402e4e3e4d8ffa
Xu Wenjing <80949489+p1anx@users.noreply.github.com>
2024-07-17 22:55:16 +0800

    Update linux_init.sh

--- linux_init.sh
+++ linux_init.sh
@@ -46,7 +46,7 @@ tar -xvf Mononoki.tar.xz -C ~/.local/share/fonts/Mononoki/
 
 # ********************************************
 # vim
-sudo apt install vim
+sudo apt install vim -y
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
