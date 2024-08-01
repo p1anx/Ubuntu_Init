bcd9831677d4290371b68b93c66e2be2708767c1
Xu Wenjing <80949489+p1anx@users.noreply.github.com>
2024-08-02 00:00:57 +0800

    Update mymihomo.sh

--- mymihomo.sh
+++ mymihomo.sh
@@ -38,4 +38,42 @@ systemctl enable mihomo
 # start mihomo
 systemctl start mihomo
 
+# set proxy on or off
+a=$(echo $SHELL)
+echo "shell:$a"
+ 
+ #if [ "$(echo $SHELL) == /bin/bash" ]; then
+ #    echo "bash"
+ #elif [ "$(echo $SEHLL) == /bin/zsh" ]; then
+ #    echo "zsh"
+ #else
+ #    echo "other"
+ #fi
+ case "$a" in
+    "/bin/bash")
+    #块写入内容
+    #以下内容必须从开头第一列开始直到EOF结尾，不然会报错
+        cat << EOF >> ~/.bashrc
+# set proxy on or off
+# 开启代理
+function pon(){
+   export all_proxy=socks5://127.0.0.1:7890  # 注意你的端口号可能不是7890，注意修改
+   export http_proxy=http://127.0.0.1:7890
+   export https_proxy=http://127.0.0.1:7890
+   echo -e "已开启代理"
+}
+# 关闭代理
+function poff(){
+    unset all_proxy
+    unset http_proxy
+    unset https_proxy
+    echo -e "已关闭代理"
+}
+EOF
+            source ~/.bashrc
+            ;;
+        *)
+            echo "other"
+            ;;
+esac
 
