bf7ad9aea25bf0029be8c71b3a08e70a47f6c7b0
xwj <2514034568@qq.com>
2024-08-02 21:22:01 +0800

    update

--- linux_init.sh
+++ linux_init.sh
@@ -6,12 +6,17 @@
 # 检测系统类型
 if grep -q "Ubuntu" /etc/os-release; then
   # Ubuntu 系统
+  echo "this is ubuntu"
   sudo apt update
-  sudo apt install -y zsh vim tmux fzf tar 
+  sudo apt install -y zsh vim tmux fzf tar wget
 elif grep -q "Rocky" /etc/os-release; then
   # Rocky Linux 系统
+  echo "this is rocky linux"
   sudo dnf update
-  sudo dnf install -y zsh vim tmux fzf tar 
+  sudo dnf install -y zsh tar wget
+  sudo dnf install -y vim
+  sudo dnf install -y tmux
+  sudo dnf install -y fzf
 else
   echo "不支持当前系统。"
   exit 1
@@ -19,9 +24,143 @@ fi
 
 echo "zsh 已成功安装。"
 #================================================
+# download mihomo file and config
+#================================================
+mihomo_dir=~/mihomo_setup
+mkdir $mihomo_dir
+cd $mihomo_dir
+system_arch=$(uname -m)
+case "$system_arch" in
+    "aarch64")
+        wget -O mihomo.gz "https://gitee.com/ClockOS/mihomo/releases/download/my_mihomo/mihomo-linux-arm64-v1.18.7.gz"
+        ;;
+    "x86_64")
+        wget -O mihomo.gz "https://gitee.com/ClockOS/mihomo/releases/download/my_mihomo/mihomo-linux-amd64-v1.18.7.gz"
+        ;;
+    *)
+        echo "mihomo config failed"
+        ;;
+esac
+
+gunzip mihomo.gz
+chmod +x mihomo
+# download clash config file
+wget -O config.yaml "https://ea8i3.no-mad-world.club/link/mnvbvGMuoVslKjYg?clash=3&extend=1"
+
+sudo mkdir /etc/mihomo
+sudo cp $mihomo_dir/mihomo /usr/local/bin
+sudo cp config.yaml /etc/mihomo
+
+#================================================
+#set mihomo.service
+mkdir $mihomo_dir/mihomo.service && cd $mihomo_dir/mihomo.service
+git clone https://gitee.com/ClockOS/mihomo.git
+sudo cp mihomo/mihomo.service /etc/systemd/system/
+
+# reload systemd
+sudo systemctl daemon-reload
+# enable mihomo
+sudo systemctl enable mihomo
+# start mihomo
+sudo systemctl start mihomo
+
+# set proxy on or off
+a=$(echo $SHELL)
+echo "shell:$a"
+ 
+ #if [ "$(echo $SHELL) == /bin/bash" ]; then
+ #    echo "bash"
+ #elif [ "$(echo $SEHLL) == /bin/zsh" ]; then
+ #    echo "zsh"
+ #else
+ #    echo "other"
+ #fi
+ case "$a" in
+    "/bin/bash")
+    #块写入内容
+    #以下内容必须从开头第一列开始直到EOF结尾，不然会报错
+        cat << EOF >> ~/.bashrc 
+# set proxy on or off
+# 开启代理
+function pon(){
+   export all_proxy=socks5://127.0.0.1:7890  # 注意你的端口号可能不是7890，注意修改
+   export http_proxy=http://127.0.0.1:7890
+   export https_proxy=http://127.0.0.1:7890
+   echo -e "已开启代理"
+}
+# 关闭代理
+function poff(){
+    unset all_proxy
+    unset http_proxy
+    unset https_proxy
+    echo -e "已关闭代理"
+}
+EOF
+        cat << EOF >> ~/.zshrc 
+# set proxy on or off
+# 开启代理
+function pon(){
+   export all_proxy=socks5://127.0.0.1:7890  # 注意你的端口号可能不是7890，注意修改
+   export http_proxy=http://127.0.0.1:7890
+   export https_proxy=http://127.0.0.1:7890
+   echo -e "已开启代理"
+}
+# 关闭代理
+function poff(){
+    unset all_proxy
+    unset http_proxy
+    unset https_proxy
+    echo -e "已关闭代理"
+}
+EOF
+            source ~/.bashrc
+            source ~/.zshrc
+            ;;
+    "/usr/bin/zsh")
+             cat << EOF >> ~/.zshrc
+# set proxy on or off
+# 开启代理
+function pon(){
+   export all_proxy=socks5://127.0.0.1:7890  # 注意你的端口号可能不是7890，注意修改
+   export http_proxy=http://127.0.0.1:7890
+   export https_proxy=http://127.0.0.1:7890
+   echo -e "已开启代理"
+}
+# 关闭代理
+function poff(){
+    unset all_proxy
+    unset http_proxy
+    unset https_proxy
+    echo -e "已关闭代理"
+}
+EOF
+            source ~/.zshrc
+            ;;
+        *)
+            echo "================================="
+            echo "error: proxy config failed!!!!!"
+            echo "================================="
+            ;;
+esac
+# 开启理
+pon
+#================================================
+# git config
+#================================================
+git config --global user.name "xwj"
+git config --global user.email "2514034568@qq.com"
+
+#================================================
 # oh my zsh
 #================================================
 sh -c "$(curl -fsSL https://gitee.com/shmhlsy/oh-my-zsh-install.sh/raw/master/install.sh)" 
+# chsh -s /usr/bin/zsh  # 将 zsh 作为默认 shell
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo 'plugins=(git z extract web-search zsh-syntax-highlighting zsh-autosuggestions)' >>~/.zshrc
+echo 'source $ZSH/oh-my-zsh.sh' >>~/.zshrc
+source ~/.zshrc
 
 #================================================
 # vim
@@ -49,103 +188,5 @@ case "$distro_name" in
     *)                                                             
         echo "other"                                               
         ;;                                                         
-esac
-#================================================
-# download mihomo file and config
-#================================================
-mkdir mihomo_setup
-cd mihomo_setup
-system_arch=$(uname -m)
-case "$system_arch" in
-    "aarch64")
-        wget -O mihomo.gz "https://gitee.com/ClockOS/mihomo/releases/download/my_mihomo/mihomo-linux-arm64-v1.18.7.gz"
-        ;;
-    "amd64")
-        wget -O mihomo.gz "https://gitee.com/ClockOS/mihomo/releases/download/my_mihomo/mihomo-linux-amd64-v1.18.7.gz"
-        ;;
-    *)
-        echo "mihomo config failed"
-        ;;
-esac
-
-gunzip mihomo.gz
-chmod +x mihomo
-# download clash config file
-wget -O config.yaml "https://ea8i3.no-mad-world.club/link/mnvbvGMuoVslKjYg?clash=3&extend=1"
-
-sudo mkdir /etc/mihomo
-sudo cp mihomo /usr/local/bin
-sudo cp config.yaml /etc/mihomo
-
-#================================================
-#set mihomo.service
-mkdir mihomo.service && cd mihomo.service
-git clone https://gitee.com/ClockOS/mihomo.git
-sudo cp mihomo/mihomo.service /etc/systemd/system/
-
-# reload systemd
-systemctl daemon-reload
-# enable mihomo
-systemctl enable mihomo
-# start mihomo
-systemctl start mihomo
-
-# set proxy on or off
-a=$(echo $SHELL)
-echo "shell:$a"
- 
- #if [ "$(echo $SHELL) == /bin/bash" ]; then
- #    echo "bash"
- #elif [ "$(echo $SEHLL) == /bin/zsh" ]; then
- #    echo "zsh"
- #else
- #    echo "other"
- #fi
- case "$a" in
-    "/bin/bash")
-    #块写入内容
-    #以下内容必须从开头第一列开始直到EOF结尾，不然会报错
-        cat << EOF >> ~/.bashrc
-# set proxy on or off
-# 开启代理
-function pon(){
-   export all_proxy=socks5://127.0.0.1:7890  # 注意你的端口号可能不是7890，注意修改
-   export http_proxy=http://127.0.0.1:7890
-   export https_proxy=http://127.0.0.1:7890
-   echo -e "已开启代理"
-}
-# 关闭代理
-function poff(){
-    unset all_proxy
-    unset http_proxy
-    unset https_proxy
-    echo -e "已关闭代理"
-}
-EOF
-            source ~/.bashrc
-            ;;
-    "/usr/bin/zsh")
-             cat << EOF >> ~/.zshrc
-# set proxy on or off
-# 开启代理
-function pon(){
-   export all_proxy=socks5://127.0.0.1:7890  # 注意你的端口号可能不是7890，注意修改
-   export http_proxy=http://127.0.0.1:7890
-   export https_proxy=http://127.0.0.1:7890
-   echo -e "已开启代理"
-}
-# 关闭代理
-function poff(){
-    unset all_proxy
-    unset http_proxy
-    unset https_proxy
-    echo -e "已关闭代理"
-}
-EOF
-            source ~/.zshrc
-            ;;
-        *)
-            echo "other"
-            ;;
 esac
 
